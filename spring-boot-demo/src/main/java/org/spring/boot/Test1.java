92c68e1df368d353593a3a2d2cca10163983545d
cherishsince <cherishsince@163.com>
2020-07-07 15:42:44 +0800

    sys

---
+++ spring-boot-demo/src/main/java/org/spring/boot/Test1.java
@@ -0,0 +1,18 @@
+package org.spring.boot;
+
+import java.util.ArrayList;
+
+/**
+ * author: Sin
+ * time: 2020/7/7 15:39
+ */
+public class Test1 {
+
+	public static void main(String[] args) {
+//		ArrayList arrayList = new ArrayList<>();
+//		arrayList.add(1);
+//		arrayList.add(2);
+//		arrayList.add(3);
+//		System.err.println(arrayList.get(-1));
+	}
+}
